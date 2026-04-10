f7337d8b96862a27e563ca9e76e0eba2a7be27c2
Vishal Kaushal <vishalkaushalit@gmail.com>
2026-04-10 13:44:00 +0530

    .

--- 21-context-api/src/App.jsx
+++ 21-context-api/src/App.jsx
@@ -1,15 +1,13 @@
-
-import Button from './components/Button'
-import Navbar from './components/Navbar'
+import Button from "./components/Button";
+import Navbar from "./components/Navbar";
 
 const App = () => {
-
   return (
     <div>
       <Navbar />
       <Button />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ 23-tanstack-query-react/src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ROUTES } from "./routes/route";
+import Login from "./auth/Login";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        
+        <Route path={ROUTES.LOGIN} element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
