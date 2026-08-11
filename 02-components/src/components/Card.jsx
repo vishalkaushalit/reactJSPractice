87b197db138bb7551c1e15a30b7fb9ab353cd966
Vishal Kaushal <vishalkaushalit@gmail.com>
2026-08-11 14:03:36 +0530

    .

--- 02-components/src/components/Card.jsx
+++ 02-components/src/components/Card.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Card = () => {
   return (
     <div>
-        <div className='card'>
-        <h1>Sarthak Sharma</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, vitae!</p>
+      <div className="card">
+        <h1>Vishal Kaushal</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt,
+          vitae!
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
